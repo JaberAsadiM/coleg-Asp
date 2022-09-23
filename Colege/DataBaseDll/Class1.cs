65bd41fc13fb6993ca6ce4fa52825581e14518c6
Jaber Asadi M <jaber.asadi.m@gmail.com>
2022-09-23 22:58:14 +0330

    DataBase dll

---
+++ Colege/DataBaseDll/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseDll
+{
+    public class Class1
+    {
+    }
+}
